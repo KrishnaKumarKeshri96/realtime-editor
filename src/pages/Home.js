b82f50b31d40cd1b2fed5a1ef256d698b106b70d
krishna <keshri819@gmail.com>
2022-04-02 01:50:13 +0530

    Created Logo, Home PAge Content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="homePageWrapper">
+      <div className="formWrapper">
+        <img
+          className="homePageLogo"
+          src="/code-sync.png"
+          alt="code-sync-logo"
+        />
+        <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+        <div className="inputGroup">
+          <input type="text" className="inputBox" placeholder="ROOM ID" />
+          <input type="text" className="inputBox" placeholder="USERNAME" />
+          <button className="btn joinBtn">Join</button>
+          <span className="createInfo">
+            If you don't have an invite then create &nbsp;
+            <button className="createNewBtn">new room</button>
+          </span>
+        </div>
+      </div>
+      <footer>
+        <h4>
+          Built with 💛 &nbsp; by &nbsp;
+          <a href="https://github.com/KrishnaKumarKeshri96">Krishna Keshri</a>
+        </h4>
+      </footer>
+    </div>
+  );
 }
 
 export default Home;
