34c8523d542884c54a8b55189b4d8df5fcc1607b
krishna <keshri819@gmail.com>
2022-04-02 01:01:10 +0530

    Components Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/Home";
+import Editor from "./pages/Editor";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Home />
+      <Editor />
     </div>
   );
 }
