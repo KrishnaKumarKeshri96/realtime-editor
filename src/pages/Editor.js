7f237c8794d60d4e1283f4e8ef6eda458575190f
krishna <keshri819@gmail.com>
2022-04-02 20:57:33 +0530

    Added Avatars in Editor PAge

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import Client from "../components/Client.jsx";
+
 function Editor() {
+  const [clients, setClients] = useState([
+    { socketId: 1, username: "Krishna K" },
+    { socketId: 2, username: "Kumar K" },
+  ]);
   return (
     <div className="mainWrap">
       <div className="aside">
@@ -7,6 +14,11 @@ function Editor() {
             <img className="logoImage" src="/logo1.png" alt="logo" />
           </div>
           <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map(({ socketId, username }) => (
+              <Client key={socketId} username={username} />
+            ))}
+          </div>
         </div>
         <button className="btn copyBtn">Copy ROOM ID</button>
         <button className="btn leaveBtn">Leave</button>
