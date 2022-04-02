370477d40d252cde4e37cfdffb6e519ebf72be3b
krishna <keshri819@gmail.com>
2022-04-02 23:29:40 +0530

    Added Code Mirror

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,5 +1,26 @@
+import { useEffect, useRef } from "react";
+import Codemirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/monokai.css";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/closebrackets";
+
 const Editor = () => {
-  return <textarea id="realtimeEditor" placeholder="textAxea"></textarea>;
+  const editorRef = useRef(null);
+  useEffect(() => {
+    async function init() {
+      Codemirror.fromTextArea(editorRef.current, {
+        mode: { name: "javascript", json: true },
+        theme: "monokai",
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+        lineNumbers: true,
+      });
+    }
+    init();
+  }, []);
+  return <textarea id="realtimeEditor" ref={editorRef}></textarea>;
 };
 
 export default Editor;
