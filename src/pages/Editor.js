b9ce74a67ab7805117904e269a36f943fd5d4edf
krishna <keshri819@gmail.com>
2022-04-02 20:47:34 +0530

    Editor Left Side Structure Created

--- src/pages/Editor.js
+++ src/pages/Editor.js
@@ -1,7 +1,19 @@
-import React from "react";
-
 function Editor() {
-  return <div>Editor</div>;
+  return (
+    <div className="mainWrap">
+      <div className="aside">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/logo1.png" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+        </div>
+        <button className="btn copyBtn">Copy ROOM ID</button>
+        <button className="btn leaveBtn">Leave</button>
+      </div>
+      <div className="editorWrap"></div>
+    </div>
+  );
 }
 
 export default Editor;
